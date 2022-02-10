0a0145153c4f8315a3eda7ce556c136a31ef9383
poi poi <bhattharshit30@gmail.com>
2022-02-10 13:10:00 +0530

    my first commit avantra

---
+++ src/Home.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import "./home.css";
+function Home() {
+  return( 
+  <div>
+      <div className="header">
+          {/* <img className="header-image" src="logo192.png" alt="" />
+          Hello */}
+      </div>
+      <div className="p1">
+          <div className="left-p">
+
+          <div className="p-1">
+              Avantra
+          </div>
+          <div className="p-2">
+              art competition
+          </div>
+          </div>
+          <div className="right-p">
+              <img src="img2.jpg" alt="" />
+          </div>
+          
+      </div>
+      <div className="p2">
+          <div className="p2-1">
+              
+              <div className="p21">
+                <div className="p21a">
+                <div className="p21a-0">
+                <div className="p21a-1">
+
+                <div className="p21-1">
+                    Avantra talent organisers
+                    associated with
+                </div>
+                <div className="p21-2">
+                    ASITIS ARTWORK
+                </div>
+
+
+
+                </div>
+              </div>
+              <div className="p22">
+                  presents art competition that is based on creatitvity, imagination, and talent of your internal artist. The motive of this Competition is to make your inner artist motivate to get outside in front of the audience, so that the people knows your talent and supports you a little up.
+              </div>
+            </div>
+                <div className="p21-3">
+                    <img src="img3.png" alt="" />
+                </div>
+
+                </div>
+              <div className="p23">
+                  <div className="p23-1">
+                      We are giving you a chance to win cash or exiciting prizes
+                  </div>
+                  <div className="p23-2">
+                      Upto Rs 10,000/-
+                  </div>
+              </div>
+              <div className="p24">
+     
+                  <div className="p24-1">
+                      This Talent Competitive Event enables all artists to show their talent on various platforms.
+
+                  </div>
+                  <div className="p24-2">
+                      so Partipate now and get into it as soon as Possible !
+                  </div>
+              </div>
+              <div className="p3">
+                  <div className="p31">
+                      Details regarding this competition : 
+                  </div>
+                  <div className="p32">
+                      <div className="p32-1">
+                          Participation Fee : 20 rupees only
+                      </div>
+                      <div className="p32-2">
+                          Submission Deadline : 20 Feb 2022
+                      </div>
+                      <div className="p32-2">
+                          Age : All age group participants can participate.
+                      </div>
+                      <div className="p32-3">
+                          <i>Kindly Submitted your Drawing and fee to provider or the person which provide you the Competition Template. </i>
+                      </div>
+                  </div>
+                    <div className="p33">
+                        <div className="p33-1">
+                        <li>Mention your name, phone number,age,in the provided form or in the drawing sheet. </li> 
+                        <li>Please submit your drawings as soon as possible before the last date of submission.</li>
+                        </div>
+                        
+                        <div className="p33-2">
+                            <div className="p33-2-1">
+                                About us:
+                            </div>
+                            <div className="p33-2-2">
+                                Avantra is Made for Talent to support it and promote it.
+                                <br /> Be with us to particiapte in more than this.
+                            </div>
+                        </div>
+                        <div className="p33-3">
+                            <div className="p33-3-1">
+                                A very thankful for the partners Asitis Artwork 
+                                to support this talent event.
+                                
+                            </div>
+                            <div className="p33-3-2">
+                                <img src="img4.jpg" alt="" />
+                                <img src="img5.jpg" alt="" />
+                            </div>
+                        </div>
+                        <div className="p34">
+                            This page is made and developed by Avanta talent organistions.
+                            <br />
+                            <div className="p34-1">
+                                <li>Bundi</li>
+                                <li>Kota</li>
+                                <li>Jaipur</li>
+                            </div>
+                            All rights are reserved | Avantra 2022 ©
+                        </div>
+                    </div>
+              </div>
+              
+          </div>
+      </div>
+  </div>
+  )
+}
+
+export default Home;
